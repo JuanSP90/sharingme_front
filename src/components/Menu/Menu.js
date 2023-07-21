78ba2543d2cf6cce2926bf242ea0e059d97cb47f
JuanSerranoP <jserpit@gmail.com>
2023-07-21 12:30:45 +0200

    menu y homepage

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -12,20 +12,49 @@ const Menu = () => {
     const { logout } = useContext(AuthContext);
 
     return (
-        <div className="navGame">
-            <div className="title">Sharing <span className="me">Me</span></div>
-            <div className={`navitems ${isOpen && "open"}`}>
-                <Link to={'/Profile'}>Profile</Link>
-                <Link to={'/Share'}>Share</Link>
-                <Link to={'/Explore'}>Explore</Link>
-                <img src={Logout} alt="logout" className="logout" onClick={logout} />
+        // <div className="navGame">
+        //     <div className="title">Sharing <span className="me">Me</span></div>
+        //     <div className={`navitems ${isOpen && "open"}`}>
+        //         <Link to={'/Profile'}>Profile</Link>
+        //         <Link to={'/Share'}>Share</Link>
+        //         <Link to={'/Explore'}>Explore</Link>
+        //         <img src={Logout} alt="logout" className="logout" onClick={logout} />
+        //     </div>
+        //     <div className={`navtoggle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)} >
+        //         <span></span>
+        //         <span></span>
+        //         <span></span>
+        //     </div>
+        // </div>
+        <nav className="navbar">
+            <div id="trapezoid">
+                <Link to="#" className="sub-home">Profile</Link>
+                <Link to="#About" className="expandHome">Share</Link>
+                <div className="subnav">
+                    <button className="subnavbtn">
+                        Clients<i className="fa fa-caret-down"></i>
+                    </button>
+                    <div className="subnav-content">
+                        {/* Contenido del submenú de Clients */}
+                    </div>
+                </div>
+                <div className="subnav">
+                    <button className="subnavbtn">
+                        Services<i className="fa fa-caret-down"></i>
+                    </button>
+                    <div className="subnav-content">
+                        <div className="subnav-trapezoid">
+                            <a href="#Services">Print Design</a>
+                            <a href="#Services">Web Design</a>
+                            <a href="#Services">Mobile App Development</a>
+                        </div>
+                    </div>
+                </div>
+                <a href="https://codepen.io/pec-man" className="expandHome">Contact</a>
             </div>
-            <div className={`navtoggle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)} >
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </div>
+            {/* Tu componente Menu */}
+            <Menu />
+        </nav>
     );
 };
 
